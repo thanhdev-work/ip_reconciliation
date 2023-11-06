72b72120d6b25041dfe72ef06639a80fa2d4fd18
thanhdev <thanhpdhe141032@gmail.com>
2023-11-06 17:35:57 +0700

    update

--- src/main/java/vn/kmedia/_2/main/Main.java
+++ src/main/java/vn/kmedia/_2/main/Main.java
@@ -1,5 +1,6 @@
 package vn.kmedia._2.main;
 
+import lombok.var;
 import vn.kmedia._1.service.CsvService;
 import vn.kmedia._2.model.RecordMissingReport;
 import vn.kmedia._2.model.RecordMissingTransactionTime;
@@ -15,7 +16,10 @@ public class Main {
     private static final String RECORD_MISSING_REPORT = "C:\\Users\\TGPM\\Documents\\WorkingSpace\\dev_km_vms_ipacking\\ip_reconciliation\\src\\main\\java\\vn\\kmedia\\_2\\resource\\record_missing_report.csv";
     private static final String RECORD_SOURCE_REPORT = "C:\\Users\\TGPM\\Documents\\WorkingSpace\\dev_km_vms_ipacking\\ip_reconciliation\\src\\main\\java\\vn\\kmedia\\_2\\resource\\record_source_report.csv";
     private static final String TRANSACTION_DETAIL_REPORT = "C:\\Users\\TGPM\\Documents\\WorkingSpace\\dev_km_vms_ipacking\\ip_reconciliation\\src\\main\\java\\vn\\kmedia\\_2\\resource\\transaction_detail_report.csv";
-    private static final DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+    private static final DateTimeFormatter inputFormatter2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
     private static final DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
 
 
@@ -32,9 +36,6 @@ public class Main {
 
         recordMissing = CsvService.toListObject(RECORD_MISSING_REPORT, RecordMissingReport.class);
         recordSource = CsvService.toListObject(RECORD_SOURCE_REPORT, RecordSourceReport.class);
-        for (RecordSourceReport e : recordSource) {
-            e.setLocalDTEndTime(LocalDateTime.parse(e.getEndTime(), inputFormatter));
-        }
         transactionDetail = CsvService.toListObject(TRANSACTION_DETAIL_REPORT, TransactionDetailReport.class);
 
         //Compare record missing vs transaction detail and get call time
@@ -42,10 +43,19 @@ public class Main {
         for (RecordMissingReport e : recordMissing) {
             for (TransactionDetailReport x : transactionDetail) {
                 if (e.getPhoneNumber().equals(x.getPhoneNumber())) {
-                    missingRecordTransTime.add(new RecordMissingTransactionTime(x.getPhoneNumber(), x.getCloseTransTime(), LocalDateTime.parse(x.getCloseTransTime(), inputFormatter)));
+                    missingRecordTransTime.add(new RecordMissingTransactionTime(x.getPhoneNumber(), x.getCloseTransTime(), LocalDateTime.parse(x.getCloseTransTime(), inputFormatter2)));
                 }
             }
         }
-
+        List<String> ok = new ArrayList<>();
+        for (RecordSourceReport e : recordSource) {
+            e.setLocalDTEndTime(LocalDateTime.parse(e.getEndTime(), inputFormatter));
+            for (RecordMissingTransactionTime x : missingRecordTransTime) {
+                LocalDateTime temp = x.getLocalDateTime().plusMinutes(10);
+                if (temp.isBefore(e.getLocalDTEndTime())) {
+                    ok.add(e.getPhoneNumber());
+                }
+            }
+        }
     }
 }
